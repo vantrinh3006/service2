03471f5a76f89bd571ba8bd5a38042793bc6a6f4
Tony Nguyen <tvtrinh@cmcglobal.vn>
2022-11-21 10:53:06 +0700

    commit 1

---
+++ src/main/java/com/example2/demo2/Student2.java
@@ -0,0 +1,8 @@
+package com.example2.demo2;
+
+public class Student2 {
+    public static void main(String[] args) {
+
+        System.out.println("This is student2 in  service2 ");
+    }
+}
---
+++ src/main/java/com/example2/demo2/Teacher2.java
@@ -0,0 +1,8 @@
+package com.example2.demo2;
+
+public class Teacher2 {
+    public static void main(String[] args) {
+
+        System.out.println("This is teacher2 in  service2 ");
+    }
+}
